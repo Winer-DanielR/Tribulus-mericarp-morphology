6964a7f65806a26a5a883f15e0e648209930633a
Winer Daniel Reyes Corral <winerdreyes@gmail.com>
2020-12-02 15:31:55 -0500

    Diagnostic function for models and to estimate kurtosis and skewness

---
+++ Scripts/02_diagnostic function_v1.R
@@ -0,0 +1,23 @@
+
+##### Diagnostic function to evaluate assumptions for linear models ####
+
+diagnostic<-function(x){
+  plot(x)
+  abline(mean(x),0)
+  hist(x)
+  qqnorm(x)
+  qqline(x,lty=2, col="Red")
+skew<-function(x){
+    m3<-sum((x-mean(x))^3)/length(x)
+    s3<-sqrt(var(x))^3
+    m3/s3}
+kurtosis<-function(x){
+    m4<-sum((x-mean(x))^4)/length(x)
+    s4<-var(x)^2
+    m4/s4-3}
+  print(paste("Kurtosis=", kurtosis(x), sep=""))
+  print(paste("Skew=", skew(x), sep=""))
+  }
+diagnostic(res)
+plot(cy_raw)
+abline(0,0, col="Red")
