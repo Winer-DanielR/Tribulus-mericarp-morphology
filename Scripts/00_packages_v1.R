87b4272050c7dc25f5e96b386ff37e76a4ac4e64
Winer Daniel Reyes Corral <winerdreyes@gmail.com>
2020-12-02 15:31:07 -0500

    Script with packages used for analysis.

---
+++ Scripts/00_packages_v1.R
@@ -0,0 +1,19 @@
+
+#################################################################
+################Tribulus Herbarium Samples#######################
+#################################################################
+
+### The main goal is to use methods to identify the samples collected on the Galapagos expedition in 2017
+### Using the Herbarium samples from the CDRS in collaboration with Patricia
+### Expedition samples are from 10 islands and mostly from 1 population per island.
+### By Daniel Reyes Corral
+### 
+
+### Package script
+
+library(tidyverse) #Data wrangling and plots
+library(lme4) #For linear models 
+library(lmerTest) #For testing models
+library(readr) #For uploading tables
+library(emmeans) #For testing models
+library(car) #For model analysis
