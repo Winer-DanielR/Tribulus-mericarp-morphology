1b1416ec5135bcb3628940a0018b8c90f788969b
Winer Daniel Reyes Corral <winerdreyes@gmail.com>
2021-02-19 12:32:03 -0500

    Edited the script to incorporate the flower and leaf datasets. Converts multiple variables into factors

--- Scripts/02_data prep_v1.R
+++ Scripts/02_data prep_v1.R
@@ -4,25 +4,66 @@
 
 ### Script for modifying data and get general data ready for analysis ###
 
+#### Mericarp data ####
+mericarp <- read_csv("C:/Users/Daniel/Documents/R/Tribulus/Tribulus mericarp morphology/Tribulus-mericarp-morphology/Data/Processed/Tribulus_mericarp_data_clean.csv")
+mericarp <- as_tibble(mericarp)
+names(mericarp)
+str(mericarp)
 
-tribulus_mericarp <- read_csv("C:/Users/Daniel/Documents/R/Tribulus/Tribulus mericarp morphology/Tribulus-mericarp-morphology/Data/Raw/Cleaned data mericarp morphology Tribulus.csv")
-tribulus_mericarp <- as_tibble(tribulus_mericarp)
-names(tribulus_mericarp)
+# Convert mericarp variables into factors
+mericarp <- mericarp %>% mutate_at(vars(ID,
+                                        Herbarium,
+                                        continent,
+                                        island_group,
+                                        mainland_island,
+                                        galapagos_other,
+                                        island_group,
+                                        galapagos_island,
+                                        finch_beak,
+                                        spine_num,
+                                        lower_spines,
+                                        country,
+                                        location), list(factor))
 
-tribulus_mericarp <- tribulus_mericarp %>% mutate_at(vars(ID,
-                                                          Herbarium,
-                                                          continent,
-                                                          island_group,
-                                                          mainland_island,
-                                                          location,
-                                                          Galapagos_island,
-                                                          finch_beak,
-                                                          site, individual_sample,
-                                                          spine_num,
-                                                          lower_spines), list(factor))
-
-str(tribulus_mericarp)
-head(tribulus_mericarp)
-
+str(mericarp)
 #Year is a number instead of a factor.
+
+
+
+#### Leaf data ####
+leaf <- read_csv("C:/Users/Daniel/Documents/R/Tribulus/Tribulus mericarp morphology/Tribulus-mericarp-morphology/Data/Processed/Tribulus_leaves_data_plus CDRS herbarium_clean.csv")
+leaf <- as_tibble(leaf)
+names(leaf)
+
+# Convert leaf variables into factors
+leaf <- leaf %>% mutate_at(vars(ID,
+                                        continent,
+                                        mainland_island,
+                                        galapagos_other,
+                                        island_group,
+                                        galapagos_island,
+                                        finch_beak,
+                                        country,
+                                        province_state,
+                                        herbarium), list(factor))
+
+str(leaf)
+
+#### Flower data ####
+flower <- read_csv("C:/Users/Daniel/Documents/R/Tribulus/Tribulus mericarp morphology/Tribulus-mericarp-morphology/Data/Processed/Tribulus_flower_data_clean.csv")
+flower <- as_tibble(flower)
+names(flower)
+
+# Covert flower data variables as factors
+flower <- flower %>% mutate_at(vars(ID,
+                                continent,
+                                mainland_island,
+                                galapagos_other,
+                                island_group,
+                                galapagos_island,
+                                finch_beak,
+                                country,
+                                province_state,
+                                Herbarium), list(factor))
+str(flower)
 
