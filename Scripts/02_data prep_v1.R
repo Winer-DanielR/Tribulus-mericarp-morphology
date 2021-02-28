b0a6a91788e7b05c3da05d741f8a561e418111c8
Winer Daniel Reyes Corral <winerdreyes@gmail.com>
2021-02-28 11:07:35 -0500

    Added individual trait databases that I transformed before for model analysis. Turn str functions into comments.

--- Scripts/02_data prep_v1.R
+++ Scripts/02_data prep_v1.R
@@ -27,7 +27,7 @@ mericarp <- mericarp %>% mutate_at(vars(ID,
                                         country,
                                         location), list(factor))
 
-str(mericarp)
+#str(mericarp)
 #Year is a number instead of a factor.
 
 #### Leaf data ####
@@ -47,7 +47,7 @@ leaf <- leaf %>% mutate_at(vars(ID,
                                         province_state,
                                         herbarium), list(factor))
 
-str(leaf)
+#str(leaf)
 
 #### Flower data ####
 flower <- read_csv("C:/Users/Daniel/Documents/R/Tribulus/Tribulus mericarp morphology/Tribulus-mericarp-morphology/Data/Processed/Tribulus_flower_data_clean.csv")
@@ -65,5 +65,114 @@ flower <- flower %>% mutate_at(vars(ID,
                                 country,
                                 province_state,
                                 Herbarium), list(factor))
-str(flower)
+#str(flower)
 
+
+
+#### Load indvidual traits and transformed dataframes ####
+# Mericarp traits ####
+meri_length <- read_csv("C:/Users/Daniel/Documents/R/Tribulus/Tribulus mericarp morphology/Tribulus-mericarp-morphology/Data/Processed/Mericarp Length.csv")
+meri_length <- meri_length %>% mutate_at(vars(ID,
+                                        Herbarium,
+                                        continent,
+                                        island_group,
+                                        mainland_island,
+                                        galapagos_other,
+                                        island_group,
+                                        galapagos_island,
+                                        finch_beak), list(factor))
+meri_width <- read_csv("C:/Users/Daniel/Documents/R/Tribulus/Tribulus mericarp morphology/Tribulus-mericarp-morphology/Data/Processed/Mericarp Width.csv")
+meri_width <- meri_width %>% mutate_at(vars(ID,
+                                              Herbarium,
+                                              continent,
+                                              island_group,
+                                              mainland_island,
+                                              galapagos_other,
+                                              island_group,
+                                              galapagos_island,
+                                              finch_beak), list(factor))
+meri_depth <- read_csv("C:/Users/Daniel/Documents/R/Tribulus/Tribulus mericarp morphology/Tribulus-mericarp-morphology/Data/Processed/Mericarp Depth.csv")
+meri_depth <- meri_depth %>% mutate_at(vars(ID,
+                                            Herbarium,
+                                            continent,
+                                            island_group,
+                                            mainland_island,
+                                            galapagos_other,
+                                            island_group,
+                                            galapagos_island,
+                                            finch_beak), list(factor))
+meri_spine.length <- read_csv("C:/Users/Daniel/Documents/R/Tribulus/Tribulus mericarp morphology/Tribulus-mericarp-morphology/Data/Processed/Mericarp Spine Length.csv")
+meri_spine.length <- meri_spine.length %>% mutate_at(vars(ID,
+                                            Herbarium,
+                                            continent,
+                                            island_group,
+                                            mainland_island,
+                                            galapagos_other,
+                                            island_group,
+                                            galapagos_island,
+                                            finch_beak), list(factor))
+meri_tip.distance <- read_csv("C:/Users/Daniel/Documents/R/Tribulus/Tribulus mericarp morphology/Tribulus-mericarp-morphology/Data/Processed/Mericarp Tip Distance.csv")
+meri_tip.distance <- meri_tip.distance %>% mutate_at(vars(ID,
+                                            Herbarium,
+                                            continent,
+                                            island_group,
+                                            mainland_island,
+                                            galapagos_other,
+                                            island_group,
+                                            galapagos_island,
+                                            finch_beak), list(factor))
+meri_spine.number <- read_csv("C:/Users/Daniel/Documents/R/Tribulus/Tribulus mericarp morphology/Tribulus-mericarp-morphology/Data/Processed/Mericarp Spine Number.csv")
+meri_spine.number <- meri_spine.number %>% mutate_at(vars(ID,
+                                            Herbarium,
+                                            continent,
+                                            island_group,
+                                            mainland_island,
+                                            galapagos_other,
+                                            island_group,
+                                            galapagos_island,
+                                            finch_beak), list(factor))
+meri_lower.spines <- read_csv("C:/Users/Daniel/Documents/R/Tribulus/Tribulus mericarp morphology/Tribulus-mericarp-morphology/Data/Processed/Mericarp Lower Spines.csv")
+meri_lower.spines <- meri_lower.spines %>% mutate_at(vars(ID,
+                                            Herbarium,
+                                            continent,
+                                            island_group,
+                                            mainland_island,
+                                            galapagos_other,
+                                            island_group,
+                                            galapagos_island,
+                                            lower_spines,
+                                            finch_beak), list(factor))
+
+# Flower traits ####
+petal_length <- read_csv("C:/Users/Daniel/Documents/R/Tribulus/Tribulus mericarp morphology/Tribulus-mericarp-morphology/Data/Processed/Flower length.csv")
+#str(petal_length)
+petal_length <- petal_length %>% mutate_at(vars(ID,
+                                                          Herbarium,
+                                                          continent,
+                                                          island_group,
+                                                          mainland_island,
+                                                          galapagos_other,
+                                                          island_group,
+                                                          galapagos_island,
+                                                          finch_beak), list(factor))
+# Leaf traits ####
+leaf_length <- read_csv("C:/Users/Daniel/Documents/R/Tribulus/Tribulus mericarp morphology/Tribulus-mericarp-morphology/Data/Processed/Leaf length.csv")
+leaf_length <- leaf_length %>% mutate_at(vars(ID,
+                                                herbarium,
+                                                continent,
+                                                island_group,
+                                                mainland_island,
+                                                galapagos_other,
+                                                island_group,
+                                                galapagos_island,
+                                                finch_beak), list(factor))
+leaflet_length <- read_csv("C:/Users/Daniel/Documents/R/Tribulus/Tribulus mericarp morphology/Tribulus-mericarp-morphology/Data/Processed/Leaflet length.csv")
+leaflet_length <- leaflet_length %>% mutate_at(vars(ID,
+                                              herbarium,
+                                              continent,
+                                              island_group,
+                                              mainland_island,
+                                              galapagos_other,
+                                              island_group,
+                                              galapagos_island,
+                                              finch_beak), list(factor))
