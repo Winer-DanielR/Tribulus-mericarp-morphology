f9538253c42d4832e82f74e85ecf2e868261aae2
Winer Daniel Reyes Corral <winerdreyes@gmail.com>
2021-12-08 12:40:22 -0500

    Added mainland-galapagos analysis

--- Scripts/04_Models per trait_v1.R
+++ Scripts/04_Models per trait_v1.R
@@ -53,6 +53,48 @@ meri_length_m3<- lmer(sqrt(length) ~ mainland_island +
 # After selecting the data transformation that converged the most I decided to
 # only show the output of that model.
 
+#### Length mainland galapagos ####
+# For length, the untransformed data seems the best
+meri_mainland_gal_length_m1<- lmer(length ~ mainland_island +
+                        year_collected +
+                        (1|ID),
+                      data = meri_length_mainland_gal,
+                      REML = F)
+meri_mainland_gal_length_m2<- lmer(log(length) ~ mainland_island +
+                        year_collected +
+                        (1|ID),
+                      data = meri_length_mainland_gal,
+                      REML = F)
+meri_mainland_gal_length_m3<- lmer(sqrt(length) ~ mainland_island +
+                        year_collected +
+                        (1|ID),
+                      data = meri_length_mainland_gal,
+                      REML = F)
+# type III test
+Anova(meri_mainland_gal_length_m1)
+#ANOVA_length_m1 <-as.data.frame(Anova(meri_length_m1))
+#summary(meri_length_m1)
+
+# Diagnostic
+# I used a custom diagnostic function and the testResiduals function
+# from the DHARMa package. This function shows the QQ residuals, dispersion test
+# and outlier tests. 
+
+# Diagnostic custom function
+#par(mfrow = c(1, 3))
+# diagnostic(resid(meri_mainland_gal_length_m1))
+# diagnostic(resid(meri_mainland_gal_length_m2))
+# diagnostic(resid(meri_mainland_gal_length_m3))
+
+# Diagnostics with DHARMA
+# testResiduals(meri_mainland_gal_length_m1)
+# testResiduals(meri_mainland_gal_length_m2)
+# testResiduals(meri_mainland_gal_length_m3)
+
+# After selecting the data transformation that converged the most I decided to
+# only show the output of that model.
+
+
 #### Width ####
 # For width, squared transformed seemed the best_
 meri_width_m1 <- lmer(width ~ mainland_island +
@@ -84,14 +126,77 @@ Anova(meri_width_m3)
 # testResiduals(meri_width_m2)
 # testResiduals(meri_width_m3)
 
+#### Width mainland galapagos ####
+# For width, log transformed seemed the best
+meri_width_mainland_gal_m1 <- lmer(width ~ mainland_island +
+                        year_collected +
+                        (1|ID),
+                      data = meri_width_mainland_gal, 
+                      REML = F)
+meri_width_mainland_gal_m2 <- lmer(log(width) ~ mainland_island +
+                        year_collected +
+                        (1|ID),
+                      data = meri_width_mainland_gal, 
+                      REML = F)
+meri_width_mainland_gal_m3 <- lmer(sqrt(width) ~ mainland_island +
+                        year_collected +
+                        (1|ID),
+                      data = meri_width_mainland_gal, 
+                      REML = F)
+# # type III test
+Anova(meri_width_mainland_gal_m2)
+# # Diagnostic
+
+# # Residual histograms
+# diagnostic(resid(meri_width_mainland_gal_m1))
+# diagnostic(resid(meri_width_mainland_gal_m2))
+# diagnostic(resid(meri_width_mainland_gal_m3))
+
+# # DHARMa
+# testResiduals(meri_width_mainland_gal_m1)
+# testResiduals(meri_width_mainland_gal_m2)
+# testResiduals(meri_width_mainland_gal_m3)
+
+#### Depth mainland galapagos ####
+# For depth untransformed data seems to work the best
+meri_depth_mainland_gal_m1 <- lmer(depth ~ mainland_island +
+                    year_collected +
+                    (1|ID),
+                    data=meri_depth_mainland_gal, 
+                    REML = F)
+meri_depth_mainland_gal_m2 <- lmer(log(depth) ~ mainland_island +
+                        year_collected +
+                        (1|ID),
+                      data=meri_depth_mainland_gal, 
+                      REML = F)
+meri_depth_mainland_gal_m3 <- lmer(sqrt(depth) ~ mainland_island +
+                        year_collected +
+                        (1|ID),
+                      data=meri_depth_mainland_gal, 
+                      REML = F)
+# type III test
+Anova(meri_depth_mainland_gal_m1)
+
+# # Diagnostic
+hist(resid(meri_depth_mainland_gal_m1), breaks = 20)
+
+# # Residual histograms
+# diagnostic(resid(meri_depth_mainland_gal_m1))
+# diagnostic(resid(meri_depth_m2))
+# diagnostic(resid(meri_depth_m3))
+# 
+# # DHARMa
+# testResiduals(meri_depth_mainland_gal_m1)
+# testResiduals(meri_depth_mainland_gal_m2)
+# testResiduals(meri_depth_mainland_gal_m3)
 
 #### Depth ####
 # For depth untransformed data seems to work the best
 meri_depth_m1 <- lmer(depth ~ mainland_island +
-                    year_collected +
-                    (1|ID),
-                    data=meri_depth, 
-                    REML = F)
+                        year_collected +
+                        (1|ID),
+                      data=meri_depth, 
+                      REML = F)
 meri_depth_m2 <- lmer(log(depth) ~ mainland_island +
                         year_collected +
                         (1|ID),
@@ -117,6 +222,7 @@ Anova(meri_depth_m1)
 # testResiduals(meri_depth_m1)
 # testResiduals(meri_depth_m2)
 # testResiduals(meri_depth_m3)
+
 
 
 #### Spine length ####
@@ -321,6 +427,58 @@ meri_tip_distance_wozero_filter <- filter(meri_tip_distance_wozero_filter,
 hist(meri_tip_distance_wozero_filter$residuals, breaks = 20)
 meri_tip_distance_wozero_filter <- filter(meri_tip_distance_wozero_filter, !is.na(residuals))
 
+
+###### Removed Zero Tip distance mainland galapagos ####
+# Raw data looks the best
+meri_tip_distance_mainland_galapagos_m4 <- lmer(tip_distance ~ mainland_island +
+                               year_collected +
+                               (1|ID),
+                             na.action = na.exclude,
+                             data=meri_tip_distance_wozero_mainland_gal,REML=F)
+
+meri_tip_distance_mainland_galapagos_m5 <- lmer(log(tip_distance + 1) ~ mainland_island +
+                               year_collected +
+                               (1|ID),
+                             data=meri_tip_distance_wozero_mainland_gal,REML=F)
+
+meri_tip_distance_mainland_galapagos_m6 <- lmer(sqrt(tip_distance) ~ mainland_island +
+                               year_collected +
+                               (1|ID),
+                             data=meri_tip_distance_wozero_mainland_gal,REML=F)
+
+
+# # type III test
+Anova(meri_tip_distance_mainland_galapagos_m4)
+Anova(meri_tip_distance_mainland_galapagos_m5)
+Anova(meri_tip_distance_mainland_galapagos_m6)
+
+
+# Diagnostic
+diagnostic(resid(meri_tip_distance_mainland_galapagos_m4))
+diagnostic(resid(meri_tip_distance_mainland_galapagos_m5))
+diagnostic(resid(meri_tip_distance_mainland_galapagos_m6))
+
+# # DHARMa
+testResiduals(meri_tip_distance_mainland_galapagos_m4)
+testResiduals(meri_tip_distance_mainland_galapagos_m5)
+testResiduals(meri_tip_distance_mainland_galapagos_m6)
+
+# Check residual distributions after removing mericarps without spines.
+
+meri_tip_distance_wozero_mainland_gal$residuals <- resid(meri_tip_distance_mainland_galapagos_m4)
+hist((meri_tip_distance_wozero_mainland_gal_filter$residuals), breaks = 20)
+
+# Filter residuals that are lower than 10
+meri_tip_distance_wozero_mainland_gal_filter <- filter(meri_tip_distance_wozero_mainland_gal,
+                                          !residuals < -5)
+meri_tip_distance_wozero_mainland_gal_filter <- filter(meri_tip_distance_wozero_mainland_gal_filter,
+                                          !residuals > 5)
+
+
+meri_tip_distance_wozero_mainland_gal_filter <- filter(meri_tip_distance_wozero_mainland_gal_filter, !is.na(residuals))
+
+
+
 ##### Filter Tip distance ####
 # Raw data works best
 
@@ -354,20 +512,60 @@ Anova(meri_tip_distance_m7)
 # DHARMa
 # testResiduals(meri_tip_distance_m7)
 
+##### Filter Tip distance mainland gal ####
+# Raw data works best
+
+meri_tip_distance_mainland_gal_m7 <- lmer(tip_distance ~ mainland_island +
+                               year_collected +
+                               (1|ID),
+                             na.action = na.exclude,
+                             data=meri_tip_distance_wozero_mainland_gal_filter,REML=F)
+
+meri_tip_distance_mainland_gal_m8 <- lmer(log(tip_distance) ~ mainland_island +
+                               year_collected +
+                               (1|ID),
+                             na.action = na.exclude,
+                             data=meri_tip_distance_wozero_mainland_gal_filter,REML=F)
+
+meri_tip_distance_mainland_gal_m9 <- lmer(sqrt(tip_distance) ~ mainland_island +
+                               year_collected +
+                               (1|ID),
+                             na.action = na.exclude,
+                             data=meri_tip_distance_wozero_mainland_gal_filter,REML=F)
+
+
+# Diagnostic
+# diagnostic(resid(meri_tip_distance_mainland_gal_m7))
+# diagnostic(resid(meri_tip_distance_mainland_gal_m8))
+# diagnostic(resid(meri_tip_distance_mainland_gal_m9))
+
+#Anova
+Anova(meri_tip_distance_mainland_gal_m7)
+
+# DHARMa
+# testResiduals(meri_tip_distance_mainland_gal_m7)
+
 #### Lower spines ####
 # meri_lower_spines_m1 <- glm(lower_spines ~ mainland_island + 
 #                                    year_collected, 
 #                              data = meri_lower_spines, 
 #                              family = "binomial")
 
-meri_lower_spines_m1_glmm <- glmmTMB(lower_spines ~ mainland_island +
+meri_lower_spines_m1_glmm <- glmmTMB(factor(lower_spines) ~ mainland_island +
                                        (1|ID),
                                      data = meri_lower_spines,
                                      family = binomial)
 
+meri_lower_spines_mainland_gal_glmm <- glmmTMB(factor(lower_spines) ~ mainland_island +
+                                       (1|ID),
+                                     data = meri_lower_spines_mainland_gal,
+                                     family = binomial)
+
+str(meri_lower_spines)
 # # Type III test
 # Anova(meri_lower_spines_m1)
 Anova(meri_lower_spines_m1_glmm)
+Anova(meri_lower_spines_mainland_gal_glmm)
 
 # Diagnostic
 # # Residual histograms
@@ -1234,6 +1432,40 @@ plot(EM_lower) + labs(title = "Mericarp Lower Spines")
 #Glm
 # EM_upper1 <- emmeans(meri_upper_spines_m1, ~ mainland_island)
 # plot(EM_upper1) + labs(title = "Mericarp Lower Spines")
+
+##### Length mainland galapagos ####
+EM_length_mainland_gal <- emmeans(meri_mainland_gal_length_m1, ~ mainland_island)
+plot(EM_length_mainland_gal, comparisons = TRUE) + labs(title = "Mericarp Length")
+pwpp(EM_length_mainland_gal)
+# ((island mean/mainland mean)-1)*100%
+((6.15/5.75 - 1)* 100) # Mericarps ~ 7% longer on islands
+
+##### Width mainland galapagos log transformed ####
+EM_width_mainland_gal <- emmeans(meri_width_mainland_gal_m2, ~ mainland_island, type = "response")
+plot(EM_width_mainland_gal, comparisons = T) + labs(title = "Mericarp Width")
+pwpp(EM_width_mainland_gal)
+((3.12/2.95 - 1)*100) # Mericarps ~ 5.76% wider on islands
+
+##### Depth mainland galapagos ####
+EM_depth_mainland_gal <- emmeans(meri_depth_mainland_gal_m1, ~ mainland_island)
+plot(EM_depth_mainland_gal, comparisons = T) + labs(title = "Mericarp Depth")
+pwpp(EM_depth_mainland_gal)
+((4.78/4.26 - 1)*100) # Mericarps ~ 12% deeper on islands
+
+##### Tip distance mainland galapagos ####
+# Zero filter data
+EM_tip_dist_mainland_gal <- emmeans(meri_tip_distance_mainland_gal_m7, ~ mainland_island)
+plot(EM_tip_dist_mainland_gal, comparisons = T) + labs(title = "Mericarp Tip distance")
+pwpp(EM_tip_dist_mainland_gal)
+((9/8.43 - 1)*100) # Mericarps ~ 6.66% more separated on islands
+
+##### Lower Spines mainland galapagos ####
+#Glmm
+EM_lower_mainland_gal <- emmeans(meri_lower_spines_mainland_gal_glmm, ~ mainland_island, type = "response")
+plot(EM_lower_mainland_gal, comparisons = T) + labs(title = "Mericarp Lower Spines")
+pwpp(EM_lower_mainland_gal)
+((0.407/0.998 - 1)*100)
+
 
 #### Flower ####
 EM_flower <- emmeans(flower_m1, ~ mainland_island)
