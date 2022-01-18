568de130dd54a8d55a08ecf17371a0be5916093c
Winer Daniel Reyes Corral <winerdreyes@gmail.com>
2022-01-18 15:27:57 -0500

    Added PCA code for leaves

--- Scripts/14_Leaf-dataset-analysis.R
+++ Scripts/14_Leaf-dataset-analysis.R
@@ -414,3 +414,111 @@ ggplot_leaflet_num3 <- ggplot(plot_leaflet_num3, aes(x = finch_beak, y = the.emm
          ", x = "Absence        Presence", 
        y = "Leaflet Number") 
 
+## Leaves ####
+
+# Preparing the data summary by ID and mainland/island column
+leaf_summary <- leaf %>% group_by(ID, mainland_island) %>% summarize(mean_leaf_length = mean(leaf_length),
+                                                                     mean_leaflet_length = mean(leaflet_length),
+                                                                     mean_leaflet_number = mean(number_of_leaflets))
+leaf_summary <- leaf_summary %>% column_to_rownames("ID")
+leaf_summary_traits <- dplyr::select(leaf_summary, !mainland_island)
+
+### PCA ####
+# leaves:
+leaf_pca <- prcomp(leaf_summary_traits, scale = T)
+
+# Visualize eigenvalues (scree plot):
+fviz_eig(leaf_pca)
+
+# Graph of individuals. Individuals with similar profile are grouped together
+fviz_pca_ind(leaf_pca, repel = T, geom = c("point"), habillage = leaf_summary$mainland_island, palette = NULL,
+             addEllipses = T, col.ind = "blue", col.ind.sup = "darkblue",
+             alpha.ind = 1, shape.ind = 19, col.quali.var = "black",
+             select.ind = list(name = NULL, cos2 = NULL, contrib = NULL),
+             gradient.cols = NULL)
+# Variables
+fviz_pca_var(leaf_pca,
+             col.var = "contrib",
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE 
+)
+#Biplot
+fviz_pca_biplot(leaf_pca, repel = T,
+                geom = c("point"),
+                habillage = leaf_summary$mainland_island,
+                col.var = "black",
+                addEllipses = T
+)
+
+# Model 2: Galapagos Other ####
+
+## Leaves ####
+# Preparing the data summary by ID and mainland/island column
+leaf_summary_model2 <- leaf_islands %>% group_by(ID, galapagos_other) %>% summarize(mean_leaf_length = mean(leaf_length),
+                                                                                    mean_leaflet_length = mean(leaflet_length),
+                                                                                    mean_leaflet_number = mean(number_of_leaflets))
+leaf_summary_model2 <- leaf_summary_model2 %>% column_to_rownames("ID")
+leaf_summary_traits_model2 <- dplyr::select(leaf_summary_model2, !galapagos_other)
+
+### PCA ####
+# leaves:
+leaf_pca_model2 <- prcomp(leaf_summary_traits_model2, scale = T)
+
+# Visualize eigenvalues (scree plot):
+fviz_eig(leaf_pca_model2)
+
+# Graph of individuals. Individuals with similar profile are grouped together
+fviz_pca_ind(leaf_pca_model2, repel = T, geom = c("point"), habillage = leaf_summary_model2$galapagos_other, palette = NULL,
+             addEllipses = T, col.ind = "blue", col.ind.sup = "darkblue",
+             alpha.ind = 1, shape.ind = 19, col.quali.var = "black",
+             select.ind = list(name = NULL, cos2 = NULL, contrib = NULL),
+             gradient.cols = NULL)
+# Variables
+fviz_pca_var(leaf_pca_model2,
+             col.var = "contrib",
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE 
+)
+#Biplot
+fviz_pca_biplot(leaf_pca_model2, repel = T,
+                geom = c("point"),
+                habillage = leaf_summary_model2$galapagos_other,
+                col.var = "black",
+                addEllipses = T
+)
+
+## Leaves ####
+# Preparing the data summary by ID and mainland/island column
+leaf_summary_model3 <- leaf_gal %>% group_by(ID, finch_beak) %>% summarize(mean_leaf_length = mean(leaf_length),
+                                                                           mean_leaflet_length = mean(leaflet_length),
+                                                                           mean_leaflet_number = mean(number_of_leaflets))
+leaf_summary_model3 <- leaf_summary_model3 %>% column_to_rownames("ID")
+leaf_summary_traits_model3 <- dplyr::select(leaf_summary_model3, !finch_beak)
+
+### PCA ####
+# leaves:
+leaf_pca_model3 <- prcomp(leaf_summary_traits_model3, scale = T)
+
+# Visualize eigenvalues (scree plot):
+fviz_eig(leaf_pca_model3)
+
+# Graph of individuals. Individuals with similar profile are grouped together
+fviz_pca_ind(leaf_pca_model3, repel = T, geom = c("point"), habillage = leaf_summary_model3$finch_beak, palette = NULL,
+             addEllipses = T, col.ind = "blue", col.ind.sup = "darkblue",
+             alpha.ind = 1, shape.ind = 19, col.quali.var = "black",
+             select.ind = list(name = NULL, cos2 = NULL, contrib = NULL),
+             gradient.cols = NULL)
+# Variables
+fviz_pca_var(leaf_pca_model3,
+             col.var = "contrib",
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE 
+)
+#Biplot
+fviz_pca_biplot(leaf_pca_model3, repel = T,
+                geom = c("point"),
+                habillage = leaf_summary_model3$finch_beak,
+                col.var = "black",
+                addEllipses = T
+)
+
