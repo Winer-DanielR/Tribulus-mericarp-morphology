2a3e620f14cb56f15d942f85c7803fec70f76a98
Winer Daniel Reyes Corral <winerdreyes@gmail.com>
2022-05-26 08:40:51 -0500

    Updated file locations. They were picking up old directory. Also updated script to filter NAs from bioclimate variables. Bio4 is not used by the paper of reference.

--- Scripts/02_Trait transformations and data prep_v1.R
+++ Scripts/02_Trait transformations and data prep_v1.R
@@ -9,7 +9,7 @@
 ## 02_01 Mericarp dataset: ####
 # Tribulus_mericarp_data_clean.csv
 
-mericarp <- read_csv("~/R/02. Thesis/Tribulus/Tribulus mericarp morphology/Tribulus-mericarp-morphology/Data/Processed/Tribulus_mericarp_data_clean.csv")
+mericarp <- read_csv("~/Vault of Ideas/20 - 29 Tribulus Research/22 Chapter. Tribulus phenotypic variation in mainland and island populations/22.03 R code/Tribulus-mericarp-morphology/Data/Processed/Tribulus_mericarp_data_clean.csv")
 mericarp <- mericarp %>% mutate_at(vars(ID,
                                               Herbarium,
                                               continent,
@@ -26,28 +26,36 @@ str(mericarp)
 # Separate each trait, remove NAs
 
 ### Length ####
-meri_length <- dplyr::select(mericarp, ind_num:mericarp_num, length)
+meri_length <- dplyr::select(mericarp, ind_num:mericarp_num, length, Temp:varP)
 meri_length <- filter(meri_length, !is.na(length))
+meri_length <- filter(meri_length, !is.na(Temp_S))
 
 ###  Width ####
-meri_width <- dplyr::select(mericarp, ind_num:mericarp_num, width)
+meri_width <- dplyr::select(mericarp, ind_num:mericarp_num, width, Temp:varP)
 meri_width <- filter(meri_width, !is.na(width))
+meri_width <- filter(meri_width, !is.na(Temp_S))
 
 ### Depth ####
-meri_depth <- dplyr::select(mericarp, ind_num:mericarp_num, depth)
+meri_depth <- dplyr::select(mericarp, ind_num:mericarp_num, depth, Temp:varP)
 meri_depth <- filter(meri_depth, !is.na(depth))
+meri_depth <- filter(meri_depth, !is.na(Temp_S))
 
 ### Spine tip distance ####
-meri_tip_distance <- dplyr::select(mericarp, ind_num:mericarp_num, tip_distance) #Has zeroes in the data
+meri_tip_distance <- dplyr::select(mericarp, ind_num:mericarp_num, tip_distance, Temp:varP) #Has zeroes in the data
 meri_tip_distance <- filter(meri_tip_distance, !is.na(tip_distance))
+meri_tip_distance <- filter(meri_tip_distance, !is.na(Temp_S))
+
 
 ### Spine tip distance without zero #####
 # We removed mericarps without upper spines from analysis.These mericarps had a tip distance of 0.
 meri_tip_distance_wozero <- dplyr::filter(meri_tip_distance, !tip_distance == 0)
 
 ### Lower spines #####
-meri_lower_spines <- dplyr::select(mericarp, ind_num:mericarp_num, lower_spines) #As factor
+meri_lower_spines <- dplyr::select(mericarp, ind_num:mericarp_num, lower_spines, Temp:varP) #As factor
 meri_lower_spines <- filter(meri_lower_spines, !is.na(lower_spines))
+meri_lower_spines <- filter(meri_lower_spines, !is.na(Temp_S))
+
+
 
 ## Traits not used in the analysis ####
 ### Spine length #####
@@ -68,7 +76,7 @@ meri_lower_spines <- filter(meri_lower_spines, !is.na(lower_spines))
 
 # 02_02 Flower dataset ####
 # Tribulus_flower_data_clean.csv
-flower <- read_csv("~/R/02. Thesis/Tribulus/Tribulus mericarp morphology/Tribulus-mericarp-morphology/Data/Processed/Tribulus_flower_data_clean.csv")
+flower <- read_csv("~/Vault of Ideas/20 - 29 Tribulus Research/22 Chapter. Tribulus phenotypic variation in mainland and island populations/22.03 R code/Tribulus-mericarp-morphology/Data/Processed/Tribulus_flower_data_clean.csv")
 flower <- flower %>% mutate_at(vars(ID,
                                         Herbarium,
                                         continent,
@@ -80,14 +88,19 @@ flower <- flower %>% mutate_at(vars(ID,
                                         galapagos_island,
                                         finch_beak), list(factor))
 str(flower)
+flower <- rename(flower, Temp = Bio_1,
+                 Temp_S = Bio_4,
+                 Prec = Bio_12,
+                 varP = Bio_15)
 
 # Remove NAs Petal length
 flower <- filter(flower, !is.na(petal_length))
+flower <- filter(flower, !is.na(Temp_S))
 
 ### Flower for Galapagos and Other Islands ####
 flower_galapagos_other <- filter(flower, !galapagos_other == "mainland")
 
-# 02_03 Galapagos only with mainland data ####
+       # 02_03 Galapagos only with mainland data ####
 # Additional analysis to test the effect of other islands by removing them from models
 
 # Made a separate mainland only dataset
