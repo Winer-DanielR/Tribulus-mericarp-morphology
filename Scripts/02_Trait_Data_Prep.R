7b832aa77e66c5fb3e76d17b676afaf3afd1adfc
Winer Daniel Reyes Corral <winerdreyes@gmail.com>
2022-12-03 13:30:39 -0500

    Kept flower analysis because we may use these results instead.

--- Scripts/02_Trait_Data_Prep.R
+++ Scripts/02_Trait_Data_Prep.R
@@ -69,7 +69,6 @@ meri_lower_spines <- filter(meri_lower_spines, !is.na(Temp_S))
 
 ## Traits not used in the analysis ####
 
-
 ### Spine length #####
 # Spine length was another trait we collected but we did not had for all samples.
 # Herbarium data was collected by Marc and during that time we did not measured spine length.
@@ -88,220 +87,27 @@ meri_lower_spines <- filter(meri_lower_spines, !is.na(Temp_S))
 
 # 02_02 Flower dataset ####
 # Tribulus_flower_data_clean.csv
-# flower <- read_csv("~/Vault of Ideas/20 - 29 Tribulus Research/22 Chapter. Tribulus phenotypic variation in mainland and island populations/22.03 R code/Tribulus-mericarp-morphology/Data/Processed/Tribulus_flower_data_clean.csv")
-# flower <- flower %>% mutate_at(vars(ID,
-#                                         Herbarium,
-#                                         continent,
-#                                         country,
-#                                         island_group,
-#                                         mainland_island,
-#                                         galapagos_other,
-#                                         island_group,
-#                                         galapagos_island,
-#                                         finch_beak), list(factor))
-# str(flower)
-# flower <- rename(flower, Temp = Bio_1,
-#                  Temp_S = Bio_4,
-#                  Prec = Bio_12,
-#                  varP = Bio_15)
-# 
+flower <- read_csv("~/Vault of Ideas/20 - 29 Tribulus Research/22 Chapter. Tribulus phenotypic variation in mainland and island populations/22.03 R code/Tribulus-mericarp-morphology/Data/Processed/Tribulus_flower_data_clean.csv")
+flower <- flower %>% mutate_at(vars(ID,
+                                        Herbarium,
+                                        continent,
+                                        country,
+                                        island_group,
+                                        mainland_island,
+                                        galapagos_other,
+                                        island_group,
+                                        galapagos_island,
+                                        finch_beak), list(factor))
+str(flower)
+flower <- rename(flower, Temp = Bio_1,
+                 Temp_S = Bio_4,
+                 Prec = Bio_12,
+                 varP = Bio_15)
+
 # # Remove NAs Petal length
-# flower <- filter(flower, !is.na(petal_length))
-# flower <- filter(flower, !is.na(Temp_S))
+flower <- filter(flower, !is.na(petal_length))
+flower <- filter(flower, !is.na(Temp_S))
 # 
 # ### Flower for Galapagos and Other Islands ####
-# flower_galapagos_other <- filter(flower, !galapagos_other == "mainland")
+flower_galapagos_other <- filter(flower, !galapagos_other == "mainland")
 
-# Other analysis subsets ####
-# 02_03 Galapagos only with mainland data ####
-# # Additional analysis to test the effect of other islands by removing them from models
-# 
-# # Made a separate mainland only dataset
-# mericarp_mainland <- filter(mericarp, mainland_island == "mainland")
-# # Made a separate Galapagos only dataset
-# gal_mericarp <- filter(mericarp, galapagos_other == "Galapagos")
-# # Combined these datasets
-# mericarp_mainland_gal <- bind_rows(mericarp_mainland, gal_mericarp)
-# 
-# # Transformed traits into factors
-# mericarp_mainland_gal <- mericarp_mainland_gal %>% mutate_at(vars(ID,
-#                                         Herbarium,
-#                                         continent,
-#                                         country,
-#                                         island_group,
-#                                         mainland_island,
-#                                         galapagos_other,
-#                                         island_group,
-#                                         galapagos_island,
-#                                         lower_spines,
-#                                         finch_beak), list(factor))
-# 
-# str(mericarp_mainland_gal)
-# 
-# 
-# ##### Length ####
-# meri_length_mainland_gal <- dplyr::select(mericarp_mainland_gal, ind_num:mericarp_num, length)
-# meri_length_mainland_gal <- filter(meri_length_mainland_gal, !is.na(length))
-# 
-# ##### Width ####
-# meri_width_mainland_gal <- dplyr::select(mericarp_mainland_gal, ind_num:mericarp_num, width)
-# meri_width_mainland_gal <- filter(meri_width_mainland_gal, !is.na(width))
-# 
-# ##### Depth ####
-# meri_depth_mainland_gal <- dplyr::select(mericarp_mainland_gal, ind_num:mericarp_num, depth)
-# meri_depth_mainland_gal <- filter(meri_depth_mainland_gal, !is.na(depth))
-# 
-# ##### Spine tip distance ####
-# meri_tip_distance_mainland_gal <- dplyr::select(mericarp_mainland_gal, ind_num:mericarp_num, tip_distance) #Has zeroes in the data
-# meri_tip_distance_mainland_gal <- filter(meri_tip_distance_mainland_gal, !is.na(tip_distance))
-# 
-# ##### Spine tip distance without zero ####
-# meri_tip_distance_wozero_mainland_gal <- dplyr::filter(meri_tip_distance_mainland_gal, !tip_distance == 0)
-# 
-# ##### Lower spines ####
-# meri_lower_spines_mainland_gal <- dplyr::select(mericarp_mainland_gal, ind_num:mericarp_num, lower_spines) #As factor
-# meri_lower_spines_mainland_gal <- filter(meri_lower_spines_mainland_gal, !is.na(lower_spines))
-
-# 02_04 Mean datasets: Estimating means from Galapagos and Florida ####
-# First I need to filter Galapagos and Florida populations since these are the ones with most data points
-# I can filter them out from the mericarp general dataset
-
-# other_mericarp <- filter(mericarp, !grepl("Florida", ID))
-# other_mericarp <- filter(other_mericarp, !grepl("Galapagos", galapagos_other))
-# # This dataset filters out Florida and Galapagos
-# 
-# galapagos_mericarp <- filter(mericarp, galapagos_other == "Galapagos")
-# # This datset is for Galapagos only data, for summary
-# 
-# florida_mericarp <- filter(mericarp, grepl("Florida", ID))
-# # This dataset is for Florida only data, for summary
-# 
-# # With each dataset separated by group I can summarize them by ID to reduce
-# # their number.Caveat: I am not summarizing lower spines here. Just the continous
-# # variables. I can ask how to summarize the lower spine data. Perhaps with proportions per ID?
-# 
-# # Summaries selects most of the column factors and estimates mean
-# # sd, se, var.
-# 
-# other_mericarp_mean <- other_mericarp %>% 
-#   group_by(ID, Herb_name, 
-#            Herb_number,
-#            Herbarium,
-#            year_collected,
-#            continent,
-#            mainland_island,
-#            galapagos_other,
-#            island_group,
-#            galapagos_island,
-#            finch_beak,
-#            country,
-#            location
-#            ) %>% 
-#   summarise_each(funs(mean,sd,var, se = sd(.)/sqrt(n()),
-#            ), length,
-#            width,
-#            depth,
-#            tip_distance,
-#            Temp,
-#            Temp_S,
-#            Prec,
-#            varP)
-# 
-# 
-# galapagos_mericarp_mean <- galapagos_mericarp %>% 
-#   group_by(ID, Herb_name, 
-#            Herb_number,
-#            Herbarium,
-#            year_collected,
-#            continent,
-#            mainland_island,
-#            galapagos_other,
-#            island_group,
-#            galapagos_island,
-#            finch_beak,
-#            country,
-#            location
-#   ) %>% 
-#   summarise_each(funs(mean,sd,var, se = sd(.)/sqrt(n()),
-#   ), length,
-#   width,
-#   depth,
-#   tip_distance,
-#   Temp,
-#   Temp_S,
-#   Prec,
-#   varP)
-# 
-# florida_mericarp_mean <- florida_mericarp %>% 
-#   group_by(ID, Herb_name, 
-#            Herb_number,
-#            Herbarium,
-#            year_collected,
-#            continent,
-#            mainland_island,
-#            galapagos_other,
-#            island_group,
-#            galapagos_island,
-#            finch_beak,
-#            country,
-#            location
-#   ) %>% 
-#   summarise_each(funs(mean,sd,var, se = sd(.)/sqrt(n()),
-#   ), length,
-#   width,
-#   depth,
-#   tip_distance,
-#   Temp,
-#   Temp_S,
-#   Prec,
-#   varP)
-# 
-# ## Adding mean datasets into a mericarp_mean dataset
-# mericarp_means <- bind_rows(galapagos_mericarp_mean,
-#                             florida_mericarp_mean,
-#                             other_mericarp_mean)
-# 
-# ## The other dataset that I could do is to integrate the galapagos
-# # and florida means into the individual other mericarp dataset.
-# 
-# galapagos_mericarp_mean <- rename(galapagos_mericarp_mean, length = length_mean,
-#                                   width = width_mean,
-#                                   depth = depth_mean,
-#                                   tip_distance = tip_distance_mean,
-#                                   Temp = Temp_mean,
-#                                   Temp_S = Temp_S_mean,
-#                                   Prec = Prec_mean,
-#                                   varP = varP_mean)
-# 
-# florida_mericarp_mean <- rename(florida_mericarp_mean, length = length_mean,
-#                                   width = width_mean,
-#                                   depth = depth_mean,
-#                                   tip_distance = tip_distance_mean,
-#                                   Temp = Temp_mean,
-#                                   Temp_S = Temp_S_mean,
-#                                   Prec = Prec_mean,
-#                                   varP = varP_mean)
-# 
-# # Create a dataset with mean galapagos and florida samples per ID, but individual samples
-# # from other mericarps
-# 
-# galapagos_mericarp_mean <- select(galapagos_mericarp_mean, c(1:21))
-# florida_mericarp_mean <- select(florida_mericarp_mean, c(1:21))
-# 
-# mericarp_ind <- bind_rows(galapagos_mericarp_mean, florida_mericarp_mean, other_mericarp)
-# mericarp_ind <- select(mericarp_ind, !c(22:31))
-# 
-# # Notes: mericarp_means has the mean by ID of all mericarps traits
-# # This condense florida, galapagos and other datasets. 
-# # mericarp_ind contains the means of galapagos and florida but the individual 
-# # measurements of the other mericarps. Which is what we could use for the review
-# # mericarp_mean has 315 observations
-# # mericarp_ind has 561 observations
-# # the galapagos_mericarp_mean have 147 observations from 3745 individual obs
-# # florida mericarp mean have 116 observations from 1124 indvidual obs
-# # other mericarps means have 52 observations our of 298 individual ones.
-# 
-# # Next steps for mean mericarps ####
-# # The meericarp dataset is the one with biased sampling. I will export these datasets
-# # and similar to the original analysis separate them in individual traits and run the models
-# 
